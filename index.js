cbbda0e575f93087ff62ccae1aea3ba3d98a09fd
Hafijur0211 <hafijur021998@gmail.com>
2024-06-03 18:36:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// Question 1
+
+// The Data Detective
+/*
+In the charming village of JavaScriptville, a young detective named Max was known for solving mysterious data cases. One day, the villagers approached Max with an intriguing challenge. They had a magical box that could contain anything - numbers, strings, arrays, or even objects. However, the villagers were unsure about the contents inside the box.
+Max's task was to determine the type of the input inside the magical box and report back with the result.
+
+Example 1:
+Input:
+"4"
+Output:
+string
+
+Example 2:
+Input:
+Hello, JavaScriptville!
+Output:
+string
+*/
+
+// Solution without function
+
+// Example inputs
+
+/*
+let input1 = "4";
+let input2 = "Hello, JavaScriptville!";
+let input3 = 123;
+let input4 = [1, 2, 3];
+let input5 = {a: 1};
+let input6 = true;
+let input7 = null;
+let input8 = undefined;
+
+Determining the type using `typeof` directly
+
+console.log(typeof input1); // Output: string
+console.log(typeof input2); // Output: string
+console.log(typeof input3); // Output: number
+console.log(typeof input4); // Output: object
+console.log(typeof input5); // Output: object
+console.log(typeof input6); // Output: boolean
+console.log(typeof input7); // Output: object (This is a quirk in JavaScript)
+console.log(typeof input8); // Output: undefined
+*/
