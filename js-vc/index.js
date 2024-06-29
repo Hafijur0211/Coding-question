d495653f2bb0322c9f23cbc66b53497d9b949789
Hafijur0211 <hafijur021998@gmail.com>
2024-06-29 09:43:34 +0530

    missing-array

--- js-vc/index.js
+++ js-vc/index.js
@@ -136,3 +136,26 @@ console.log(findItem)
 */
 
 // ------------------------------------------------------------------------------------------------------------------------------------
+
+// 6. How to find the missing numbers in a given integer array of 1 to 10 
+
+/*
+
+const arrNumbers = [1,2,3,4,5,6,9,10]
+const missArray=[]
+const missingValue=(arr)=>{
+const minValue= Math.min(...arr)
+const maxValue=Math.max(...arr)
+for(let i = minValue; i<maxValue; i++){
+  if(arr.indexOf(i)<0){
+    missArray.push(i)
+  }
+}
+return(missArray)
+}
+
+console.log(missingValue(arrNumbers))
+
+*/
+
+// ------------------------------------------------------------------------------------------------------------------------------------
