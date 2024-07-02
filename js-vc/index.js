7341a9bfb8201ecb304330c6f10d705bc82d39ec
Hafijur0211 <hafijur021998@gmail.com>
2024-07-02 11:04:57 +0530

    compare

--- js-vc/index.js
+++ js-vc/index.js
@@ -441,3 +441,26 @@ console.log(`${num1} ${operator} ${num2} = ${result}`);
 */
 
 // ------------------------------------------------------------------------------------------------------------------------------------
+
+// 18. How to compare two arrays are equal or not in javascript
+
+/*
+
+var arr1 = [2, 9, 6, 8, 4];
+var arr2 = [4, 8, 6, 9, 12];
+
+const isArrSame =
+  arr1.length == arr2.length &&
+  // indexOf method return -1 if element is not exist in array
+  // every method check each and every element fullfil the condition exist in array
+  arr1.every((curEle) => {
+    if (arr2.indexOf(curEle) > -1) {
+      return (curEle = [arr2.indexOf(curEle)]);
+    }
+  });
+console.log(isArrSame);
+
+
+*/
+
+// ------------------------------------------------------------------------------------------------------------------------------------
